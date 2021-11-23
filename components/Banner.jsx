03e14e0e286f4026f462fba16c0613a50d4f3235
Tristan Deane <59102133+IM-Deane@users.noreply.github.com>
2021-11-22 19:33:32 -0700

    Added store cards, temp data, and home pg styling

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -7,7 +7,7 @@ function Banner(props) {
 				<span className={styles.title1}>Caffeine</span>
 				<span className={styles.title2}>Addicts</span>
 			</h1>
-			<p className={styles.subtitle}>Discover your local coffee shops!</p>
+			<p className={styles.subTitle}>Discover your local coffee shops!</p>
 			<div className={styles.buttonWrapper}>
 				<button className={styles.button} onClick={props.handleOnClick}>
 					{props.buttonText}
--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,20 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 import Banner from "../components/Banner";
+import Card from "../components/Card";
 
-export default function Home() {
+import coffeeStoresData from "../data/coffee-stores.json";
+
+export async function getStaticProps(context) {
+	return {
+		// will be passed to the page component as props
+		props: {
+			coffeeStores: coffeeStoresData,
+		},
+	};
+}
+
+export default function Home(props) {
 	const handleOnBannerBtnClick = () => {
 		console.log("hi from the banner button");
 	};
@@ -21,18 +33,36 @@ export default function Home() {
 			</Head>
 
 			<main className={styles.main}>
-				<Banner
-					buttonText="View stores nearby"
-					handleOnClick={handleOnBannerBtnClick}
-				/>
-				<div className={styles.heroImage}>
-					<Image
-						src="/static/hero-img.png"
-						width={700}
-						height={400}
-						alt="Caffeine Addicts landing picture"
+				<header className={styles.mainHeader}>
+					<Banner
+						buttonText="View stores nearby"
+						handleOnClick={handleOnBannerBtnClick}
 					/>
-				</div>
+					<div className={styles.heroImage}>
+						<Image
+							src="/static/hero-img.png"
+							width={700}
+							height={400}
+							alt="Caffeine Addicts landing picture"
+						/>
+					</div>
+				</header>
+				<h2 className={styles.heading2}>Edmonton Coffee Shops</h2>
+				{props.coffeeStores.length > 0 ? (
+					<div className={styles.cardLayout}>
+						{props.coffeeStores.map((store, idx) => (
+							<Card
+								key={store.id}
+								className={styles.card}
+								name={store.name}
+								href={`/coffee-store/${store.id}`}
+								imgUrl={store.imgUrl}
+							/>
+						))}
+					</div>
+				) : (
+					<p>No shops found!</p>
+				)}
 			</main>
 		</div>
 	);
